0f6b7e1f419ea96da5483df1c853eb16f729bf4f
Anders Sandström <anders.sandstrom@esss.se>
2021-12-08 10:34:08 +0100

    WIP

--- iocsh/st.cmd
+++ iocsh/st.cmd
@@ -7,8 +7,8 @@ iocshLoad "$(TOP)/iocrc.iocsh"
 # [IOC] core
 iocshLoad "$(mrfioc2_DIR)/evr.iocsh"      "P=$(PEVR),PCIID=$(PCIID),EVRDB=$(EVRDB=evr-mtca-300-univ.db)"
 iocshLoad "$(mrfioc2_DIR)/evrevt.iocsh"   "P=$(PEVR),$(EVREVTARGS=)"
-mrmEvrLoopback EVR,1,1
-time2ntp("EVR", 2)
+#mrmEvrLoopback EVR,1,1
+#time2ntp("EVR", 2)
 
 iocInit
 
@@ -18,3 +18,6 @@ iocshLoad "$(mrfioc2_DIR)/evrdlygen.r.iocsh" "P=$(PEVR),$(EVRDLYGENARGS=)"
 iocshLoad "$(mrfioc2_DIR)/evrout.r.iocsh" "P=$(PEVR),$(EVROUTARGS=)"
 iocshLoad "$(mrfioc2_DIR)/evrin.r.iocsh" "P=$(PEVR),$(EVRINARGS=)"
 iocshLoad "$(TOP)/evrLb.test.r.iocsh" "P=$(PEVR)"
+mrmEvrLoopback EVR,1,1
+time2ntp("EVR", 2)
+
