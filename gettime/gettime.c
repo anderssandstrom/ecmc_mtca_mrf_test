80cc4c27c86ca82ac37afb6f1ae33703d50f0cfa
Anders Sandström <anders.sandstrom@esss.se>
2022-06-07 17:07:20 +0200

    Add clock_monotonic_raw to gettime

--- gettime/gettime.c
+++ gettime/gettime.c
@@ -29,20 +29,20 @@ int timespec2str(char *buf, uint len, struct timespec *ts) {
 
 main()
 {
-  struct timespec mono,real;
+  struct timespec mono,real,raw;
   const uint TIME_FMT = strlen("2012-12-31 12:59:59.123456789") + 1;
   char timestr[TIME_FMT];
 
   clock_gettime( CLOCK_REALTIME, &real);
   clock_gettime( CLOCK_MONOTONIC, &mono);
-  
+  clock_gettime( CLOCK_MONOTONIC_RAW, &raw);
   //CLOCK_REALTIME
   if (timespec2str(timestr, sizeof(timestr), &real) != 0) {
     printf("timespec2str failed!\n");
     return EXIT_FAILURE;
   } else {
     unsigned long resol = real.tv_sec * NANO + real.tv_nsec;
-    printf("CLOCK_REALTIME:  %s (raw=%ld ns)\n", timestr,resol);
+    printf("CLOCK_REALTIME:      %s (raw=%ld ns)\n", timestr,resol);
   }
 
   //CLOCK_MONOTONIC
@@ -51,7 +51,16 @@ main()
     return EXIT_FAILURE;
   } else {
     unsigned long resol = mono.tv_sec * NANO + mono.tv_nsec;
-    printf("CLOCK_MONOTONIC: %s (raw=%ld ns)\n", timestr,resol);
+    printf("CLOCK_MONOTONIC:     %s (raw=%ld ns)\n", timestr,resol);
+  }
+
+  //CLOCK_MONOTONIC_RAW
+  if (timespec2str(timestr, sizeof(timestr), &raw) != 0) {
+    printf("timespec2str failed!\n");
+    return EXIT_FAILURE;
+  } else {
+    unsigned long resol = raw.tv_sec * NANO + raw.tv_nsec;
+    printf("CLOCK_MONOTONIC_RAW: %s (raw=%ld ns)\n", timestr,resol);
   }
 
   return EXIT_SUCCESS;
